e1504b45e7ed86e98a758e13e4b63f2c0112dc0c
Glider <2929799602@qq.com>
2024-01-08 22:59:30 +0800

    feat: 数据库添加登陆创建用户

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,62 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
+import { Prisma, User as UserModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
+
+  @Get('user/:id')
+  async getUserById(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @Post('user/getUsers')
+  async getUsers(
+    @Body()
+    searchInfo: {
+      email: string;
+    },
+  ): Promise<UserModel[]> {
+    const { email } = searchInfo;
+    return this.userService.users({
+      where: email
+        ? {
+            email,
+          }
+        : {},
+    });
+  }
+
+  // 创建用户
+  @Post('user/createUser')
+  async createUser(
+    @Body()
+    userData: {
+      name?: string;
+      age?: number;
+      gender?: boolean;
+      email: string;
+    },
+  ): Promise<UserModel> {
+    const { name, age, gender, email } = userData;
+    return this.userService.createUser({
+      name,
+      age,
+      gender,
+      email,
+    });
+  }
+
+  // 删除用户
+  @Delete('user/:id')
+  async deletePost(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
+  }
 
   @Get()
   getHello(): string {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginService } from './user/login.service';
+import { UserService } from './user.service';
 import { UserController } from './user/login.controller';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UserController],
-  providers: [AppService],
+  providers: [AppService, LoginService, UserService, PrismaService],
 })
 export class AppModule {}
